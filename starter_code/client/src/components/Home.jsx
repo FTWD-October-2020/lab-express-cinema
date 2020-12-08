a1389b2737e1afb44c4391d0746096972b4ce26f
Cameron Campbell <cameroncampbell@camerons-air.attlocal.net>
2020-12-08 09:57:00 -0500

    cam

--- starter_code/client/src/components/Home.jsx
+++ starter_code/client/src/components/Home.jsx
@@ -1,18 +1,30 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import axios from "axios";
 
-export default class Home extends Component {
-  async callMyBackend() {
+export default function Home(props) {
+  const [movies, setMovies] = useState(false);
+  function callMyBackend() {
     // this is an example call to our backend
-    let responseFromBackend = await axios.get("http://localhost:5000/");
-    console.log(responseFromBackend);
+    axios.get("http://localhost:5000/movies").then((response) => {
+      setMovies(response.data);
+    });
   }
-  render() {
+  console.log(movies);
+
+  function listMovieData() {
     return (
-      <div>
-        <button onClick={this.callMyBackend}>Test Backend Call</button>
-        <h1>Welcome to Express Cinema</h1>
-      </div>
+      movies &&
+      movies.map((movie) => {
+        return <h1>{movie.title}</h1>;
+      })
     );
   }
+
+  return (
+    <div>
+      <button onClick={callMyBackend}>Test Backend Call</button>
+      <h1>Welcome to Express Cinema</h1>
+      {listMovieData()}
+    </div>
+  );
 }
