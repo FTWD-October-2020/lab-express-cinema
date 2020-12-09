699c5dc1dc4ce28616661fd6121edea0a01ca444
Zach Conley <zach@zachs-air.myfiosgateway.com>
2020-12-09 09:53:10 -0500

    zach's changes

--- starter_code/client/src/components/CreateNewMovie.jsx
+++ starter_code/client/src/components/CreateNewMovie.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Axios from 'axios';
 import { useState } from "react";
 
 
@@ -7,13 +8,24 @@ const CreateNewMovie = () => {
         title: ""
     })
     function handleChange(event) {
-        setMovie({ title: event.target.value });
+        setMovie({
+            ...movie,
+             [event.target.name]: event.target.value });
+    }
+
+    const submit = () => {
+        Axios.post("http://localhost:5000/movies/new", movie)
+        .then(res => console.log(res, "We posted a movie"))
+        .catch(err => console.log(err, "We've hit an error"))
     }
 
     return (
         <div>
-            <input type="text" placeholder="movie name" value={movie.title} onChange={handleChange} />
-            <button>Create</button>
+            <input type="text" name="title" placeholder="title" value={movie.title} onChange={handleChange} />
+            <input type="text" name="director" placeholder="director" value={movie.director} onChange={handleChange} />
+            <input type="text" name="description" placeholder="description" value={movie.description} onChange={handleChange} />
+            <input type="text" name="image" placeholder="image url" value={movie.image} onChange={handleChange} />
+            <button onClick={submit}>Create</button>
         </div>
     );
 };
